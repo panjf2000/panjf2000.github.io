41dead88671a4b7407f3b795b0b2dff591f8645d
Andy Pan <panjf2000@gmail.com>
2020-07-08 13:30:47 +0800

    rebuilding site Wed Jul  8 13:30:47 CST 2020

--- dp.sh
+++ dp.sh
@@ -2,6 +2,8 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+git pull --rebase
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
@@ -16,8 +18,6 @@ if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
-
-git pull --rebase
 
 # Push source and build repos.
 git push origin master
